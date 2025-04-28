ad297d50d624343f7365e142407c92702f8cc147
Luciano Fustinoni <119234077+luchofustinoni@users.noreply.github.com>
2025-04-28 12:20:27 -0300

    Primer commit

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,37 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-doble :: Int -> Int
-doble x = x * 2
+
+
+Modelo de los poderes
+
+data Poder = UnPoder {
+    nombreDelPoder :: String, 
+    tipoDelPoder :: String,
+    puntosQueQuita :: Int,
+    radioDeExplosion :: Int,
+    activaSuperpoder :: Bool,
+}
+
+poderBolaEspinosa :: Poder;
+poderBolaEspinosa UnaCantidadDe= 
+
+
+
+/-----/
+
+data Personaje = UnPersonaje {
+    nombrePersonaje :: String,
+    poderBasico :: String,
+    superpoder :: String, 
+    poderActivo :: Bool, 
+    cantidadDeVida :: Int,
+}
+
+primerPersonaje :: Personaje
+primerPersonaje = UnPersonaje "Espina" "Bola de espinas" "Granada de espinas" true 4800
+
+segundoPersonaje :: Personaje
+segundoPersonaje = UnPersonaje "Pamela" "Lluvia de tuercas sanadoras" "Torreta curativa" false 9600
+
+/-----/
